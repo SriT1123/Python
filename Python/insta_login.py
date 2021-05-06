00388355356489fa6ecaef42eef95c580f9afb73
Sri T <66740689+SriT1123@users.noreply.github.com>
2021-05-05 22:27:56 -0400

    universal application

--- Python/insta_login.py
+++ Python/insta_login.py
@@ -4,10 +4,10 @@ driver = webdriver.Chrome()
 driver.get('https://instagram.com')
 
 user_box = driver.find_element_by_xpath('//*[@id="loginForm"]/div/div[1]/div/label/input')
-user_box.send_keys('srinand._')
+user_box.send_keys('enter username here')
 
 pass_box = driver.find_element_by_xpath('//*[@id="loginForm"]/div/div[2]/div/label/input')
-pass_box.send_keys('N@ndlp23')
+pass_box.send_keys('enter password here')
 
 log_button = driver.find_element_by_xpath('//*[@id="loginForm"]/div/div[3]/button')
 log_button.click()
